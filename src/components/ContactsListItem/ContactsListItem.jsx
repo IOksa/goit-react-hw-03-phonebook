d4a1540cd4ceb275e49c179283cf49e5666108e5
IOksa <117656767+IOksa@users.noreply.github.com>
2023-06-17 18:46:45 +0300

    update

--- src/components/ContactsListItem/ContactsListItem.jsx
+++ src/components/ContactsListItem/ContactsListItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import css from './ContactsListItem.module.css';
 
 const ContactsListItem = ({contact:{id, name, number}, onDeleteContact}) => {
@@ -17,5 +18,16 @@ return (
     </>
     );                  
 };
+
+ContactsListItem.propTypes={
+    contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        name:  PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+    }).isRequired,
+    ),
+    onDeleteContact: PropTypes.func.isRequired,
+}; 
 
 export default ContactsListItem;
